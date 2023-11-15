56144ac7549da6131d197b9c59d03b092304380a
aldershko <iamalengeorge7@gmail.com>
2023-11-15 16:34:02 +0530

    refactor: import bird and plane model

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import planeScene from '../assets/3d/plane.glb'
+import { useGLTF } from '@react-three/drei'
 
 const Plane = () => {
+    const{scene , animations} = useGLTF(planeScene)
   return (
-    <div>Plane</div>
+    <mesh>
+        <primitive object={scene} />
+    </mesh>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import  Islando  from '../models/Island'
 import Sky from '../models/Sky'
+import Bird from '../models/Bird'
+import Plane from '../models/Plane'
 
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>POPUP</div> */}
 const Home = () => {
@@ -31,12 +33,14 @@ const Home = () => {
                 <ambientLight intensity={0.5}/>
                
                 <hemisphereLight skyColor='#b1e1ff' groundColor="#000000" intensity={1} />
+                <Bird />
                 <Sky />
                 <Islando
                 scale={islandScale}
                 position={islandPosition}
                 rotation={islandRotation}
                  />
+                <Plane />
             </Suspense>
 
         </Canvas>
