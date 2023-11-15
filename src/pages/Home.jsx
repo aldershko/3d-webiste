9b9bd724e2084b77dafd17267b898d997fd92b94
aldershko <iamalengeorge7@gmail.com>
2023-11-15 15:59:22 +0530

    refactor:import the 3d model and add lighting and scaling to it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
+import  Islando  from '../models/Island'
 
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>POPUP</div> */}
 const Home = () => {
+    const adjustIslandForScreenSize = () =>{
+        let islandScale = null;
+        let islandPosition = [0,-6.5,-43]
+        let rotation  = [0.1,4.7,0]
+
+        if(window.innerWidth < 768){
+            islandScale = [0.9,0.9,0.9]
+        }else{
+            islandScale = [1,1,1]
+        }
+        return [islandScale,islandPosition,rotation]
+    }
+    const [islandScale,islandPosition ,islandRotation] = adjustIslandForScreenSize();
   return (
     <section className='w-full h-screen relative'>
         <Canvas 
@@ -12,11 +26,15 @@ const Home = () => {
         camera={{near: 0.1, far: 1000}}
         >
             <Suspense fallback = {<Loader />}>
-                <directionalLight />
-                <ambientLight />
-                <pointLight />
-                <spotLight />
-                <hemisphereLight />
+                <directionalLight position={[1,1,1]} intensity={2} />
+                <ambientLight intensity={0.5}/>
+               
+                <hemisphereLight skyColor='#b1e1ff' groundColor="#000000" intensity={1} />
+                <Islando
+                scale={islandScale}
+                position={islandPosition}
+                rotation={islandRotation}
+                 />
             </Suspense>
 
         </Canvas>
