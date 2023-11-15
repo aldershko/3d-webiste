f0cf66fbd023a33ef423807a16a98ba4022f954d
aldershko <iamalengeorge7@gmail.com>
2023-11-15 10:46:02 +0530

    refactor: add routing using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import {Route,BrowserRouter as Router,Routes} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <h3 className='text-lg text-red-700'>Hello</h3>
-    </div>
+    <main className='bg-slate 300/20'>
+      <Router>
+        Navbar
+        <Routes>
+          <Route path='/' element={'Home'}></Route>
+          <Route path='/about' element={'About'}></Route>
+          <Route path='/projects' element={'Projects'}></Route>
+          <Route path='/contacts' element={'Contacts'}></Route>
+
+        </Routes>
+      </Router>
+    </main>
   )
 }
 
