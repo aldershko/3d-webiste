b3e3a8c7e1a7487e71d9ade89e49250c1f57d3e0
aldershko <iamalengeorge7@gmail.com>
2023-11-15 12:30:46 +0530

    refactor: add the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import {Route,BrowserRouter as Router,Routes} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+
 
 const App = () => {
   return (
     <main className='bg-slate 300/20'>
       <Router>
-        Navbar
+        <Navbar />
         <Routes>
-          <Route path='/' element={'Home'}></Route>
-          <Route path='/about' element={'About'}></Route>
-          <Route path='/projects' element={'Projects'}></Route>
-          <Route path='/contacts' element={'Contacts'}></Route>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/projects' element={<Projects />}></Route>
+          <Route path='/contacts' element={<Contact />}></Route>
 
         </Routes>
       </Router>
